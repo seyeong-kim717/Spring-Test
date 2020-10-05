5dddf915e4eb46414c235a7df92173340dd8b132
irene <sykim1123@nate.com>
2020-10-05 12:52:16 +0900

    노트북사용 spring05

---
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
@@ -0,0 +1,22 @@
+package com.gura.spring05.cafe.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.gura.spring05.cafe.dto.CafeCommentDto;
+import com.gura.spring05.cafe.dto.CafeDto;
+
+public interface CafeService {
+	public void getList(HttpServletRequest request);
+	public void getDetail(HttpServletRequest request);
+	public void saveContent(CafeDto dto);
+	public void updateContent(CafeDto dto);
+	public void deleteContent(int num, HttpServletRequest request);
+	public void saveComment(HttpServletRequest request);//댓글 저장 
+	public void deleteComment(HttpServletRequest request);//댓글 삭제
+	public void updateComment(CafeCommentDto dto);//댓글 수정
+	public void moreCommentList(HttpServletRequest request);//댓글 추가 응답
+	public Map<String, Object> getList2(HttpServletRequest request);
+}
---
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -0,0 +1,35 @@
+package com.gura.spring05.users.service;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring05.users.dto.UsersDto;
+
+public interface UsersService {
+	public Map<String, Object> isExistId(String inputId);
+	public void addUser(UsersDto dto);
+	public void loginProcess(UsersDto dto, ModelAndView mView,
+			HttpSession session);
+	public void getInfo(HttpSession session, ModelAndView mView);
+	public void deleteUser(HttpSession session);
+	public Map<String, Object> saveProfileImage(HttpServletRequest request,
+			MultipartFile mFile);
+	public void updateUser(HttpSession session, UsersDto dto);
+	public void updateUserPwd(HttpSession session, UsersDto dto,
+			ModelAndView mView);
+	public Map<String, Object> ajaxLoginProcess(UsersDto dto, HttpSession session);
+	public boolean jsonpLogin(UsersDto dto);
+}
+
+
+
+
+
+
+
+
